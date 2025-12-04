f7d00aa68f33acabb3d02e5f1e95107d6e65ac2d
Ozair0101 <ozairjan186@gmail.com>
2025-12-04 16:09:50 +0430

    a

---
+++ app/Http/Controllers/Api/ApiController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use Illuminate\Http\JsonResponse;
+use Illuminate\Routing\Controller as BaseController;
+
+class ApiController extends BaseController
+{
+    protected function success($data = null, string $message = '', int $status = 200): JsonResponse
+    {
+        return response()->json([
+            'success' => true,
+            'data' => $data,
+            'message' => $message,
+        ], $status);
+    }
+
+    protected function error(string $message, int $status = 400, $errors = null): JsonResponse
+    {
+        return response()->json([
+            'success' => false,
+            'message' => $message,
+            'errors' => $errors,
+        ], $status);
+    }
+}
