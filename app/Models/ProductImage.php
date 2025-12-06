3bc85852756efbc0acef47f125f424cf097a33c0
Ozair0101 <ozairjan186@gmail.com>
2025-12-06 14:01:50 +0430

    p

---
+++ app/Models/ProductImage.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class ProductImage extends Model
+{
+    //
+    protected $fillable = [
+        'url',
+        'is_primary',
+        'product_id'
+    ];
+
+    public function product(): BelongsTo
+    {
+        return $this->belongsTo(Product::class, 'product_id', 'product_id');
+    }
+}
