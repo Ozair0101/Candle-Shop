7b7111c5585d69063de99423fe285ca3ee063ad7
Ozair0101 <ozairjan186@gmail.com>
2025-12-22 11:32:46 +0430

    p

---
+++ app/Models/ProductReview.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class ProductReview extends Model
+{
+    protected $fillable = [
+        'product_id',
+        'user_id',
+        'name',
+        'rating',
+        'comment',
+    ];
+
+    public function product(): BelongsTo
+    {
+        return $this->belongsTo(Product::class, 'product_id', 'product_id');
+    }
+}
